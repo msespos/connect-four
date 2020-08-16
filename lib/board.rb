4304bbb75b050a1440ad9c554c9034be868ae67c
Mike Esposito <mikeesposito6852@gmail.com>
2020-08-15 19:04:04 -0700

    Add tests for @board and add @board to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -2,4 +2,8 @@
 
 # board class
 class Board
+
+  def initialize
+    @board = Array.new(7) { Array.new(6) { '-' } }
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,6 +5,26 @@
 require_relative '../lib/board.rb'
 
 RSpec.describe Board do
-
   subject(:board) { Board.new }
+  describe '@board' do
+    context 'when the board is instantiated' do
+      it 'is a 6 row board' do
+        expect(board.instance_variable_get(:@board)[0].size).to eq(6)
+      end
+
+      it 'is a 7 column board' do
+        expect(board.instance_variable_get(:@board).size).to eq(7)
+      end
+
+      it 'has "-" in the bottom left spot' do
+        expect(board.instance_variable_get(:@board)[0][0]).to eq("-")
+      end
+
+      it 'has "-" in the bottow right spot' do
+        expect(board.instance_variable_get(:@board)[6][0]).to eq("-")
+      end
+    end
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
