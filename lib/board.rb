717b90ce11009d8ac171e7e87cdd7489fc9e97b3
Mike Esposito <mikeesposito6852@gmail.com>
2020-08-20 11:54:44 -0700

    Refactor Board methods

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,10 @@
 
 # board class
 class Board
+  COLUMN_SHIFTS = [1, 0, 1, -1].freeze
+  ROW_SHIFTS = [0, 1, -1, -1].freeze
+  WIN_TYPES = %i[row column negative_slope_diagonal positive_slope_diagonal].freeze
+
   def initialize
     board
   end
@@ -20,8 +24,7 @@ class Board
 
   # determine if the board is in a red win, yellow win, or no win yet state
   def board_status
-    win_types = %i[row column negative_slope_diagonal positive_slope_diagonal]
-    win_types.each do |win_type|
+    WIN_TYPES.each do |win_type|
       return winner_or_none(win_type) if winner_or_none(win_type) != :no_winner_yet
     end
     :no_win_yet
@@ -41,41 +44,48 @@ class Board
     :error
   end
 
-  # used by #board_status; determines the winner if there is a winner at this point in the game
+  # used by #board_status to determine the winner if there is a winner at this point in the game
   def winner_or_none(win_type)
     @board.each_with_index do |column, column_index|
       column.each_index do |row_index|
         next if @board[column_index][row_index] == '-'
 
-        case win_type
-        when :row
-          next if column_index > 3
-        when :column
-          next if row_index > 2
-        when :negative_slope_diagonal
-          next if row_index < 3 || column_index > 3
-        when :positive_slope_diagonal
-          next if row_index < 3 || column_index < 3
-        end
+        next if indices_within_threshold(column_index, row_index, win_type)
+
         return symbol(@board[column_index][row_index]) if four_consecutive?(column_index, row_index, win_type)
       end
     end
     :no_winner_yet
   end
 
-  # used by #winner_or_none; converts a string on the board into a symbol
+  # used by #winner_or_none to determine if the indices being examined are within the
+  # appropriate threshold for the win_type
+  def indices_within_threshold(column_index, row_index, win_type)
+    case win_type
+    when :row
+      column_index > 3
+    when :column
+      row_index > 2
+    when :negative_slope_diagonal
+      row_index < 3 || column_index > 3
+    when :positive_slope_diagonal
+      row_index < 3 || column_index < 3
+    end
+  end
+
+  # used by #winner_or_none to convert a string on the board into a symbol
   def symbol(string)
     string == 'r' ? :red : :yellow
   end
 
-  # used by #winner_or_none; checks four spots given an initial spot to see if they are
+  # used by #winner_or_none to check four spots given an initial spot to see if they are
   # consecutive of one color
   def four_consecutive?(column_index, row_index, win_type)
     first_spot, second_spot, third_spot, fourth_spot = win_spots(column_index, row_index, win_type)
     first_spot == second_spot && first_spot == third_spot && first_spot == fourth_spot
   end
 
-  # used by #four_consecutive?; creates the four spots to be checked using column and row
+  # used by #four_consecutive? to create the four spots to be checked using column and row
   # shifts as appropriate to the type of win
   def win_spots(column_index, row_index, win_type)
     column_shift, row_shift = shifts(win_type)
@@ -86,21 +96,15 @@ class Board
     [first_spot, second_spot, third_spot, fourth_spot]
   end
 
-  # used by #win_spots; generates the appropriate column and row shifts according to win_type
+  # used by #win_spots to generate the appropriate column and row shifts according to win_type
   def shifts(win_type)
-    case win_type
-    when :row
-      column_shift = 1
-      row_shift = 0
-    when :column
-      column_shift = 0
-      row_shift = 1
-    when :negative_slope_diagonal
-      column_shift = 1
-      row_shift = -1
-    when :positive_slope_diagonal
-      column_shift = -1
-      row_shift = -1
+    column_shift = 0
+    row_shift = 0
+    WIN_TYPES.each_with_index do |w_type, index|
+      if w_type == win_type
+        column_shift = COLUMN_SHIFTS[index]
+        row_shift = ROW_SHIFTS[index]
+      end
     end
     [column_shift, row_shift]
   end
