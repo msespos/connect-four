e1b9674c557305d38283f7337b9951ceb6c7a1ce
Mike Esposito <mikeesposito6852@gmail.com>
2020-08-19 17:53:21 -0700

    Refactor Board class

--- lib/board.rb
+++ lib/board.rb
@@ -16,6 +16,13 @@ class Board
     @board[column][row_to_use(column)] = token
   end
 
+  def win_status
+    row_status ||
+      column_status ||
+      negative_slope_diagonal_status ||
+      positive_slope_diagonal_status
+  end
+
   def row_status
     win?(:row) ? :win : :no_win_yet
   end
@@ -47,17 +54,18 @@ class Board
   def win?(win_type)
     @board.each_with_index do |column, column_index|
       column.each_index do |row_index|
+        next if @board[column_index][row_index] == '-'
+
         case win_type
         when :row
-          next if column_index > 3 || @board[column_index][row_index] == '-'
+          next if column_index > 3
         when :column
-          next if row_index > 2 || @board[column_index][row_index] == '-'
+          next if row_index > 2
         when :negative_slope_diagonal
-          next if row_index < 3 || column_index > 3 || @board[column_index][row_index] == '-'
+          next if row_index < 3 || column_index > 3
         when :positive_slope_diagonal
-          next if row_index < 3 || column_index < 3 || @board[column_index][row_index] == '-'
+          next if row_index < 3 || column_index < 3
         end
-
         return true if four_consecutive?(column_index, row_index, win_type)
       end
     end
@@ -69,26 +77,30 @@ class Board
     first_spot == second_spot && first_spot == third_spot && first_spot == fourth_spot
   end
 
-  def win_spots(column_index, row_index, win_type)
-    first_spot = @board[column_index][row_index]
+  def shifts(win_type)
     case win_type
     when :row
-      second_spot = @board[column_index + 1][row_index]
-      third_spot = @board[column_index + 2][row_index]
-      fourth_spot = @board[column_index + 3][row_index]
+      column_shift = 1
+      row_shift = 0
     when :column
-      second_spot = @board[column_index][row_index + 1]
-      third_spot = @board[column_index][row_index + 2]
-      fourth_spot = @board[column_index][row_index + 3]
+      column_shift = 0
+      row_shift = 1
     when :negative_slope_diagonal
-      second_spot = @board[column_index + 1][row_index - 1]
-      third_spot = @board[column_index + 2][row_index - 2]
-      fourth_spot = @board[column_index + 3][row_index - 3]
+      column_shift = 1
+      row_shift = -1
     when :positive_slope_diagonal
-      second_spot = @board[column_index - 1][row_index - 1]
-      third_spot = @board[column_index - 2][row_index - 2]
-      fourth_spot = @board[column_index - 3][row_index - 3]
+      column_shift = -1
+      row_shift = -1
     end
+    [column_shift, row_shift]
+  end
+
+  def win_spots(column_index, row_index, win_type)
+    column_shift, row_shift = shifts(win_type)
+    first_spot = @board[column_index][row_index]
+    second_spot = @board[column_index + column_shift][row_index + row_shift]
+    third_spot = @board[column_index + column_shift * 2][row_index + row_shift * 2]
+    fourth_spot = @board[column_index + column_shift * 3][row_index + row_shift * 3]
     [first_spot, second_spot, third_spot, fourth_spot]
   end
 end
